e164a273bf0e8447a7e93c7062d8bf815de3d945
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-06-01 09:55:48 +0200

    feat. canceled slug row in update controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->orderBy('projects.created_at', 'desc')->paginate(6);
+        $projects = Project::with('type', 'technologies')->orderBy('projects.created_at', 'desc')->paginate(10);
+
+        $technologies = Technology::all();
 
         return response()->json([
             'success' => true,
-            'result' => $projects
+            'result' => $projects,
+            'allTechnology' => $technologies,
         ]);
     }
 
